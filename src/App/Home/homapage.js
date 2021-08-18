b91c8eff9d5553c416ba5e186898d03b8cf6476d
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2021-08-18 19:32:00 +0430

    homepage style

--- src/App/Home/homapage.js
+++ src/App/Home/homapage.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import Information from '../Information/information'
+import './homepage.css'
+import {cars} from '../Functions/carArray'
 
 export default function Homapage() {
+
+    const [selectedCar , setSelectedCar] = useState(null)
+    const selectedTagValue = useRef()
+
     return (
         <React.Fragment>
+
+            <input list="Cars" ref={selectedTagValue} className="car-select-tag" placeholder="Select Your Car" />
+            <datalist id="Cars">
+                {cars.map(car=><option key={car.name} value={car.name}>{car.name}</option>)}
+            </datalist>
+
+            <button className="confirm-car" onClick={()=>setSelectedCar(selectedTagValue.current.value===''?null:selectedTagValue.current.value)}>Confirm Car</button>
             
+            <Information car={selectedCar} />
+
         </React.Fragment>
     )
 }
