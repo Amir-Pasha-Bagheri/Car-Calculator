23eef85aed348f45fe8fddea3eca8535253bff83
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2021-08-18 20:02:26 +0430

    return all the information to client

--- src/App/Functions/returnCarInformation.js
+++ src/App/Functions/returnCarInformation.js
@@ -6,7 +6,14 @@ export default function ReturnCarInformation({selectedCarInfo}) {
     })
     return (
         <div className="car-information">
-            <p className="info-title"><span>Name</span> {selectedCarInfo.name}</p>
+            <p className="info-title"><span>Name</span> <span style={{color:'brown'}}>{selectedCarInfo.name}</span></p>
+            <p className="info-title"><span>Engine Volume (cc)</span> {selectedCarInfo.engineVolume}</p>
+            <p className="info-title"><span>Acceleration (s)</span> {selectedCarInfo.acceleration}</p>
+            <p className="info-title"><span>Max Speed (km/h)</span> {selectedCarInfo.maxSpeed}</p>
+            <p className="info-title"><span>Weight (kg)</span> {selectedCarInfo.weight}</p>
+            <p className="info-title"><span>Fuel Consumtion (L/100km)</span> {selectedCarInfo.fuel}</p>
+            <p className="info-title"><span>Tank Volume (L)</span> {selectedCarInfo.tankVolume}</p>
+            <p className="info-title"><span>Price (Rial)</span> {selectedCarInfo.price}</p>
         </div>
     )
 }
