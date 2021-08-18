103b62aa1efec289674bdff10939184b2289992a
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2021-08-18 20:01:58 +0430

    complete information for each car

--- src/App/Functions/carArray.js
+++ src/App/Functions/carArray.js
@@ -1,5 +1,32 @@
 export const cars = [
-    {name:"Peraid"},
-    {name:"Peugeot 405"},
-    {name:"Samand"}
+    {
+        name:"Peraid",
+        engineVolume:1323,  //cc
+        acceleration:15.5,  //s
+        maxSpeed:156,       //km/h
+        weight:850,         //kg
+        fuel:6.8,           //L/100km
+        tankVolume: 37,     //L
+        price : 120000000   //Rial
+    },
+    {
+        name:"Peugeot 405",
+        engineVolume:1761,
+        acceleration:11,
+        maxSpeed:190, 
+        weight:100, 
+        fuel:9.1, 
+        tankVolume: 70, 
+        price : 200000000 
+    },
+    {
+        name:"Samand",
+        engineVolume:1800,
+        acceleration:14.5,
+        maxSpeed:845, 
+        weight:1220, 
+        fuel:8, 
+        tankVolume: 70, 
+        price : 186000000
+    }
 ]
