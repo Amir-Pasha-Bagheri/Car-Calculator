b48c7f231f1b316a3c6b48b4e13411dc0ae3aa66
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2021-08-19 21:51:37 +0430

    improve UI

--- src/App/Navbar/navbar.js
+++ src/App/Navbar/navbar.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import './navbar.css'
 
-export default function Navbar() {
+export default function Navbar({component,path}) {
     const [openNav,setOpenNav] = useState(false)
     return (
         <div className={openNav?"default-navbar open":"default-navbar"}>
             <span className="nav-button" onClick={()=>setOpenNav(!openNav)}>{openNav?"⇩":"⇧"}</span>
-            <Link to="/a"><button className="link-to-other-page">Compare Cars</button></Link>
+            <Link to={path}><button className="link-to-other-page">{component}</button></Link>
             <button className="link-to-other-page disabled">More Coming Soon...</button>
         </div>
     )
