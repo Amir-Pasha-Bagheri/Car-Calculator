abfe82b23a66a3e47da838b48df6293e68d7abd8
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2021-08-19 21:53:32 +0430

    change render component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom'
 import history from "./history";
 import './index.css';
-import Homepage from './App/Home/homapage';;
+//import Homepage from './App/Home/homepage';
+import Compare from './App/Compare/compare'
 
 ReactDOM.render(
   <Router history={history}>
-    <Homepage />
+    <Compare />
   </Router>,
   document.getElementById('root')
 );
