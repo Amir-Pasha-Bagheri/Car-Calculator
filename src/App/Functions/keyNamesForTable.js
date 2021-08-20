d501b6b33a2ec26effee494e689d1f9b2b6fc1a7
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2021-08-20 18:58:05 +0430

    set keynames for more formal table

---
+++ src/App/Functions/keyNamesForTable.js
@@ -0,0 +1,10 @@
+export const keyNamesForClient = {
+    name:"Name",
+    engineVolume:"Engine Volume",
+    acceleration:"Acceleration",
+    maxSpeed:"Max Speed", 
+    weight:"Wieght", 
+    fuel:"Fuel", 
+    tankVolume: "Tank Volume", 
+    price : "Price" 
+}
